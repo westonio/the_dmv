be415d383fe8339f3b182a24f863eadeea8d8481
Weston Schutt <wtschutt@gmail.com>
2023-05-17 15:27:46 -0600

    Feature: Add @license_data initialized variable

--- lib/registrant.rb
+++ lib/registrant.rb
@@ -1,10 +1,11 @@
 class Registrant
-  attr_reader :name, :age, :permit
+  attr_reader :name, :age, :permit, :license_data
 
   def initialize(name, age, permit = false)
     @name = name
     @age = age
     @permit = permit
+    @license_data = {:written=>false, :license=>false, :renewed=>false}
   end
 
   def permit?
