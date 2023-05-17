df39bf24af7e43d73e517f932ddbd548844535da
Weston Schutt <wtschutt@gmail.com>
2023-05-17 15:22:24 -0600

    Add tests for initialized variable license_data and .earn_permit method

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -30,4 +30,19 @@ RSpec.describe Registrant do
 
     expect(registrant_1.permit?).to eq(true)
   end
+
+  it 'has default license data' do
+    registrant_1 = Registrant.new('Bruce', 18, true)
+    registrant_2 = Registrant.new('Penny', 15)
+
+    expect(registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+    expect(registrant_2.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+  end
+
+  it 'has .earn_permit method' do
+    registrant_2 = Registrant.new('Penny', 15)
+
+    registrant_2.earn_permit
+    expect(registrant_2.permit?).to eq(true)
+  end
 end
