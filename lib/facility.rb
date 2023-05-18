3219b23573b83367e00014a72ecdc08568c83759
Weston Schutt <wtschutt@gmail.com>
2023-05-17 18:41:35 -0600

    Feature: Add new instance variables and .register_vehicle method

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,14 +1,24 @@
 class Facility
-  attr_reader :name, :address, :phone, :services
+  attr_reader :name, :address, :phone, :services, :collected_fees, :registered_vehicles
 
   def initialize(name:, address:, phone:)
     @name = name
     @address = address
     @phone = phone
     @services = []
+    @collected_fees = 0
+    @registered_vehicles = []
   end
 
   def add_service(service)
     @services << service
+  end
+
+  def register_vehicle(vehicle)
+    if @services.include?('Vehicle Registration')
+      @registered_vehicles << vehicle
+    else
+      "Error: Vehicle Registration is not enabled for this facility"
+    end
   end
 end
