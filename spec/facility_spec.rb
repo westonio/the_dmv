1225ffbf2a11f6b4a4a1aa85162f73d3f6e2a354
Weston Schutt <wtschutt@gmail.com>
2023-05-17 18:34:19 -0600

    Add tests for new instance variables and method .register_vehicle

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -11,6 +11,8 @@ RSpec.describe Facility do
       expect(@facility.address).to eq('2242 Santiam Hwy SE Albany OR 97321')
       expect(@facility.phone).to eq('541-967-2014')
       expect(@facility.services).to eq([])
+      expect(@facility.collected_fees).to eq(0)
+      expect(@facility.registered_vehicles).to eq([])
     end
   end
 
@@ -21,6 +23,15 @@ RSpec.describe Facility do
       @facility.add_service('Renew Drivers License')
       @facility.add_service('Vehicle Registration')
       expect(@facility.services).to eq(['New Drivers License', 'Renew Drivers License', 'Vehicle Registration'])
+    end
+  end
+
+  describe '#register vehicle' do
+    it 'can register a vehicle' do
+      @facility.add_service('Vehicle Registration')
+      camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice})
+      @facility.register_vehicle(camaro)
+      expect(@facility.registered_vehicles).to eq([camaro])
     end
   end
 end
