97273e4a0a70b7448536cdc9555866edfef06f70
Weston Schutt <wtschutt@gmail.com>
2023-05-18 14:21:01 -0600

    Feature: Add .administer_road_test method

--- lib/facility.rb
+++ lib/facility.rb
@@ -52,4 +52,12 @@ class Facility
       registrant.license_data[:written]
     end
   end
+
+  def administer_road_test(registrant)
+    if @services.include?('Road Test') && registrant.license_data[:written] == true
+      registrant.license_data[:license] = true
+    else
+      registrant.license_data[:license]
+    end
+  end
 end
