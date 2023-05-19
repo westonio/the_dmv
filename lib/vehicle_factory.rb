416bc88a7a80e5aa0a8491bf789946145d939a50
Weston Schutt <wtschutt@gmail.com>
2023-05-19 13:50:30 -0600

    Fix: Update formatting of .create_vehicles method to meet ruby conventions

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -2,12 +2,13 @@ class VehicleFactory
 
   def create_vehicles(vehicle_dataset)
     vehicle_dataset.map do |vehicle_details|
-      parsed_data = {vin: vehicle_details[:vin_1_10],
+      parsed_data = {
+      vin: vehicle_details[:vin_1_10],
       year: vehicle_details[:model_year],
       make: vehicle_details[:make],
       model: vehicle_details[:model],
-      engine: :ev}
-
+      engine: :ev
+    }
       Vehicle.new(parsed_data)
     end
   end
