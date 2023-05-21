11c20356755c0449d3545d044a212db9bba941ad
Weston Schutt <wtschutt@gmail.com>
2023-05-20 19:14:21 -0600

    Refactor: update facility_spec test suite

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -3,6 +3,9 @@ require 'spec_helper'
 RSpec.describe Facility do
   before(:each) do
     @facility = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
+    @cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
+    @bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
+    @camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice} )
   end
   describe '#initialize' do
     it 'can initialize' do
@@ -29,90 +32,77 @@ RSpec.describe Facility do
   describe '#charge_fee' do
     it 'can charge 200 for EV' do
       @facility.add_service('Vehicle Registration')
-      bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
 
-      expect(@facility.charge_fee(bolt)).to eq(200)
+      expect(@facility.charge_fee(@bolt)).to eq(200)
     end
 
     it 'can charge 25 for antique' do
       @facility.add_service('Vehicle Registration')
-      camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice})
       
-      expect(@facility.charge_fee(camaro)).to eq(25)
+      expect(@facility.charge_fee(@camaro)).to eq(25)
     end
 
     it 'can charge 100 for all others' do
       @facility.add_service('Vehicle Registration')
-      cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
 
-      expect(@facility.charge_fee(cruz)).to eq(100)
+      expect(@facility.charge_fee(@cruz)).to eq(100)
     end
   end
 
   describe '#add_plate' do
     it 'can add its plate' do
       @facility.add_service('Vehicle Registration')
-      cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
-      camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice})
-      bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
-
-      expect(@facility.add_plate(cruz)).to eq(:regular)
-      expect(@facility.add_plate(camaro)).to eq(:antique)
-      expect(@facility.add_plate(bolt)).to eq(:ev)
+      
+      expect(@facility.add_plate(@cruz)).to eq(:regular)
+      expect(@facility.add_plate(@camaro)).to eq(:antique)
+      expect(@facility.add_plate(@bolt)).to eq(:ev)
     end
   end
 
   describe '#register_vehicle' do
     it 'can register a vehicle' do
       @facility.add_service('Vehicle Registration')
-      camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice})
-      @facility.register_vehicle(camaro)
-      expect(@facility.registered_vehicles).to eq([camaro])
+      @facility.register_vehicle(@camaro)
+      expect(@facility.registered_vehicles).to eq([@camaro])
     end
     
     it 'gives error if facility does not offer service' do
-      camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice})
-      expect(@facility.register_vehicle(camaro)).to eq("Error: Vehicle Registration is not enabled for this facility")
+      expect(@facility.register_vehicle(@camaro)).to eq("Error: Vehicle Registration is not enabled for this facility")
     end
 
     it 'adds $200 to fees for electric vehicles' do
       @facility.add_service('Vehicle Registration')
-      bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
-      @facility.register_vehicle(bolt)
+      @facility.register_vehicle(@bolt)
 
       expect(@facility.collected_fees).to eq(200)
     end
 
     it 'adds $25 to fees for cars over 25 years' do
       @facility.add_service('Vehicle Registration')
-      camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice})
-      @facility.register_vehicle(camaro)
+      @facility.register_vehicle(@camaro)
 
       expect(@facility.collected_fees).to eq(25)
     end
 
     it 'adds $100 to fees for all others' do
       @facility.add_service('Vehicle Registration')
-      cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
-      @facility.register_vehicle(cruz)
+      @facility.register_vehicle(@cruz)
 
       expect(@facility.collected_fees).to eq(100)
     end
 
     it 'can update the registration date' do
       @facility.add_service('Vehicle Registration')
-      camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice})
-      @facility.register_vehicle(camaro)
+      @facility.register_vehicle(@camaro)
 
-      expect(camaro.registration_date).to eq(Date.today)
+      expect(@camaro.registration_date).to eq(Date.today)
     end
 
     it 'can update the plate type' do
       @facility.add_service('Vehicle Registration')
-      camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice})
-      @facility.register_vehicle(camaro)
+      @facility.register_vehicle(@camaro)
 
-      expect(camaro.plate_type).to eq(:antique)
+      expect(@camaro.plate_type).to eq(:antique)
     end
   end
 
